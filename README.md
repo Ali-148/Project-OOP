114e4c8afa5c37f793e014d1e839a8a0ea66e9ff
Muhammad Ali Farooq <81547153+Ali-148@users.noreply.github.com>
2022-08-10 12:15:30 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+### Implement the Project According to the specification given on google classroom.
+
+## Make the following folders
+### 1. UML
+### 2. Task List
+### 3. Code
+
+### Make sure you make submissions in respected folders.
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "Functions.h"
+
+TEST_CASE( "2: Boundary Cases", "[multi-file:2]" ) {
+    
+}
+
+TEST_CASE( "2: Valid Cases", "[multi-file:2]" ) {
+   
+}
